48bfaa33f7396fc68be3a920a8694ac8f0729c36
CiroFiniello <kirofiniello@outlook.it>
2024-12-23 20:43:39 +0100

    aggiunta dell'.env al gitignore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "./header/Header.jsx";
 import Dashboard from "./dashboard/Dashboard.jsx";
-// import CityWeather from "./cityweather/CityWeather.jsx";
+import CityWeather from "./cityweather/CityWeather.jsx";
 import "./App.css";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/home" component={Dashboard} />
-        {/* <Route path="/city/:id" component={CityWeather} /> */}
+        <Route path="/city/:id" element={<CityWeather />} />
       </Routes>
     </Router>
   );
--- src/context/WeatherContext.jsx
+++ src/context/WeatherContext.jsx
@@ -1,36 +1,23 @@
 // src/context/WeatherContext.js
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 // Creiamo il contesto
-export const WeatherContext = createContext();
+const WeatherContext = createContext();
 
 const WeatherProvider = ({ children }) => {
-  // Definiamo lo stato che vogliamo condividere
-  const [cities, setCities] = useState([
-    { id: "Rome", name: "Roma" },
-    { id: "Milan", name: "Milano" },
-    { id: "Naples", name: "Napoli" },
-    { id: "Turin", name: "Torino" },
-    { id: "Palermo", name: "Palermo" },
-  ]);
-  const [cityWeather, setCityWeather] = useState(null);
+  const [weatherData, setWeatherData] = useState({});
 
-  // // Funzione per cercare le informazioni della città
-  // const getCityWeather = async (id) => {
-  //   const apiKey = 'TUA_API_KEY';
-  //   const response = await fetch(
-  //     `http://api.openweathermap.org/data/2.5/weather?q=${id}&appid=${apiKey}&units=metric&lang=it`
-  //   );
-  //   const data = await response.json();
-  //   setCityWeather(data);
-  // };
+  // Funzione per ottenere i dati meteo
+  const fetchWeatherData = async (city) => {
+    // Inserire qui la logica per chiamare l'API di OpenWeather e aggiornare lo state
+  };
 
   // Il provider fornisce il contesto ai componenti figli
   return (
-    <WeatherContext.Provider value={{ cities, cityWeather }}>
+    <WeatherContext.Provider value={{ weatherData, fetchWeatherData }}>
       {children}
     </WeatherContext.Provider>
   );
 };
 
-export default WeatherProvider;
+export { WeatherContext, WeatherProvider };
